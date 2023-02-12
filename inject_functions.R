1f0bfa523d9d4a1243bb4ac7c683f78b30ceb4ac
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-02-12 18:26:40 +0100

    init inject_functions

---
+++ inject_functions.R
@@ -0,0 +1,258 @@
+# This is a library of functions used for structure testing and injecting new data 
+# into the database
+# Depends on: stringr, dbplyr, DBI, RSQLite
+
+# Regex patterns
+regex_matches_study_code <- "^[a-zA-Z]+_[12][0-9][0-9][0-9]_[a-zA-Z]+$"
+
+# Loop check vector of column names in data frame
+do_elements_exist <- function(colnames, object){
+  vec = c()
+  for (i in seq_along(colnames))
+  {
+    vec[i] = exists(colnames[i], object)
+    if (!exists(colnames[i], object))
+    {
+      warning(paste("Column", colnames[i], "not present in data specified"))
+    } 
+  }
+  return(all(vec))
+}
+
+# Object entries - this is for checking the list name, if they contain proper structure
+check_object_elements <- function(object){
+  names = names(object)
+  no_study = names[which(names != "study")]
+  if (!"study" %in% names)
+  {
+    stop("Object needs to have a 'study' element")
+  } else if (!all(stringr::str_detect(names, "^data_[0-9]+$|study")))
+  {
+    error_name = names[which(stringr::str_detect(names, "^data_[0-9]+$|study", negate = TRUE))]
+    error_message = paste(
+      "Element-name:",
+      error_name,
+      "invalid.",
+      "Elements can only be named 'study' or 'data_[NUMBER]"
+    )
+    stop(error_message)
+  } else if (!any(stringr::str_detect(no_study, "^data_[0-9]+$")))
+  { 
+    error_name = names
+    error_message = paste(
+      "Object must contain at least one data element named 'data_[NUMBER].",
+      "Current names:",
+      error_name
+    )
+    stop(error_message) 
+  } else if (any(duplicated(names)))
+  {
+    error_name = names[which(duplicated(names))]
+    error_message = paste(
+      "Element-name:",
+      error_name,
+      "is not unique.",
+      "Elements in object must be uniquely named."
+    )
+    stop(error_message)
+  }# TODO: Hard check names 'study', 'data_1', 'data_2' depending on length of list
+  {
+    return(TRUE)
+  }
+}
+
+# check study info structure
+check_study_info_structure <- function(object){
+  if(is.data.frame(object$study) == FALSE)
+  {# check study info is data frame
+    stop("Study-Info is not a dataframe")
+  } 
+  else if (nrow(object$study) != 1){# check the number of rows in that dataframe, should be 1
+    stop("Study-Info contains more than one row")
+  } 
+  else if (do_elements_exist(c("study_code", "study_info"), object$study) == FALSE){
+    stop("Need to have variables: study_code, study_info present")
+  }
+  else if (stringr::str_detect(get_study_code(object),
+                               regex_matches_study_code,
+                               negate = TRUE))
+  {#does study code match regex pattern
+    stop(paste("Study code:",
+               get_study_code(object),
+               "is not valid. Study codes should follow the principle AUTHOR_YEAR_FIRSTWORD"))
+  }
+  else {
+    return(TRUE)
+  }
+}
+
+# Make two: Check raw data, check overview structure
+
+# Check data list structure
+check_data_structure <- function(object){
+  # Check if all other (not study) objects are lists containing two 1 items
+  for (i in 2:length(object)){
+    if(inherits(object[[i]], "list") == FALSE)
+    {
+      stop(paste("Error in data entry", i - 1, ":", "Data entry in object must be a list of two elements (data, overview)"))
+    } else if(length(object[[i]]) != 2)
+    {
+      stop(paste("Error in data entry", i - 1, ":", "Data entry in object must be a list of two elements (data, overview)"))
+    } else if (!do_elements_exist(c("data", "overview"), object[[i]]))
+    {
+      stop(paste("Error in data entry", i - 1, ":", "Data entry in object must be a list of two elements (data, overview)"))
+    } else if(!is.data.frame(object[[i]]$data))
+    {
+      stop(paste("Error in data entry", i - 1, ":", "Raw data must be a data frame"))
+    } else if(!is.data.frame(object[[i]]$overview))
+    {
+      stop(paste("Error in data entry", i - 1, ":", "Overview must be a data frame"))
+    } else if (!do_elements_exist(c("subject", "trial", "accuracy", "rt"), object[[i]]$data))
+    {
+      stop(paste("Error in data entry", i - 1, ":", "Raw data must have correct columns specified"))
+    } else if (!do_elements_exist(c("task_name", "size"), object[[i]]$overview))
+    {
+      stop(paste("Error in data entry", i - 1, ":", "Overview must have correct columns specified"))
+    } else
+    {
+      return(TRUE)
+    }
+  }
+}
+
+# Check object structure
+check_object_structure <- function(object){
+  if (inherits(object, "list") == FALSE)
+  {
+    stop("Object not a list")
+  } 
+  check_object_elements(object)
+  check_study_info_structure(object)
+  check_data_structure(object)
+}
+
+# Returns vector of task names found in inject object
+get_task_names <- function(object){
+  n_lists = length(object) - 1
+  task_names = c()
+  for (i in 1:n_lists){
+    task_names[i] = object[[i+1]][[2]]$task_name
+  }
+  return(task_names)
+}
+
+# Returns vector of study code specified in object
+get_study_code <- function(object){
+  study_code = c()
+  study_code = object$study$study_code
+  return(study_code)
+}
+
+# Checks to see if a given study_code exists in study-lookup table
+does_study_id_exist <- function(conn, object){
+  code = get_study_code(object)
+  study_table = tbl(conn, "study")
+  study_id = study_table %>% 
+    filter(study_code == code) %>% 
+    pull(study_id)
+  length = length(study_id)
+  if (length == 0){
+    return(FALSE)
+  } else if (length == 1){
+    return(TRUE)
+  } else {
+    stop("This study code was already found twice in the database. Please investigate what went wrong here.")
+  }
+}
+
+does_study_id_exist(con, object)
+
+# Add a study variable to database
+add_study_info <- function(conn, object){ # add more study variables here
+  insert_study_info = list(
+    object$study$study_code, 
+    object$study$study_info
+  )
+  return(
+    dbSendQuery(
+      conn,
+      'INSERT INTO study (study_code, study_info) VALUES (?, ?);',
+      insert_study_info
+    )
+  )
+}
+
+# Find study id of table for a given code
+find_study_id <- function(conn, object){
+  code = get_study_code(object)
+  study_table = tbl(conn, "study")
+  study_id = study_table %>% 
+    filter(study_code == code) %>% 
+    pull(study_id)
+  return(study_id)
+}
+
+# Finds the study id in table of that code
+return_study_id <- function(conn, object){
+  if (does_study_id_exist(conn, object) == FALSE) 
+  {
+    warning("This study information does not currently exist, adding it to the study-table")
+    # TODO: Make person confirm this? Show them the info that's added
+    add_study_info(conn, object)
+    find_study_id(conn, object)
+  } else 
+  {
+    find_study_id(conn, object)
+  }
+}
+
+
+# Checks to see if a given task_name exists in task-lookup table
+does_task_id_exist <- function(conn, object){
+  names = get_task_names(object)
+  task_table = tbl(conn, "task")
+  vec = c()
+  for (i in seq_along(names))
+  {
+    temp_task_name = names[i]
+    task_id = task_table %>% 
+      filter(task_name == temp_task_name) %>% 
+      pull(task_id)
+    length = length(task_id)
+    if (length == 0){
+      vec[i] = FALSE
+      stop(paste("Task Name:", temp_task_name, "not found in database"))
+    } else if (length == 1){
+      vec[i] = TRUE
+    } else {
+      stop("This task name was already found twice in the database. Please investigate what went wrong here.")
+    }
+  }
+  return(all(vec))
+}
+
+# Option to add new task?
+
+# Find study id of table for a given code, returns data_frame of 
+find_task_id <- function(conn, object){
+  names = get_task_names(object)
+  task_table = tbl(conn, "task")
+  df = data.frame(task_id = c(), task_name = c())
+  for (i in seq_along(names))
+  {
+    temp_task_name = names[i]
+    task_id = task_table %>% 
+      filter(task_name == temp_task_name) %>% 
+      select(task_id, task_name) %>% 
+      as_tibble()
+    df = rbind(df, task_id)
+  }
+  return(df)
+}
+
+# Finds the task id in table of that code
+return_task_id <- function(conn, object){
+  does_task_id_exist(conn, object)
+  find_task_id(conn, object)
+}
+
