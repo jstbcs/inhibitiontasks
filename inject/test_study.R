0369e85149610cdcc7c789a94fde59467fe70542
MadlenHoffstadt <80783145+MadlenHoffstadt@users.noreply.github.com>
2023-04-04 11:20:15 +0200

    added correct_n_of_withinid() and check_data_list()

--- inject/test_study.R
+++ inject/test_study.R
@@ -13,6 +13,7 @@ source("helper_functions.R")
 
 # This function checks whether an element on the data-level consists of one task element,
 # one overview element, one data element, and one within element
+
 correct_elements_in_data_list <- function(object){
   names = names(object)
   
@@ -134,6 +135,51 @@ correct_cols_in_within_info <- function(within_info){
   if (length(missing_cols) > 0){
     stop(c("Columnname(s) missing from within_info data frame: ", paste(missing_cols, collapse = ", ")))
   }
+}
+
+
+# This function checks whether the number of rows in the within_info table equals the 
+# number of within conditions coded in the data table and whether there are duplicate within_ids 
+# in within_id
+correct_n_of_withinid <- function(within_info, data_df){
+  # check if within ids are unique
+  if(length(unique(within_info$within_id)) != nrow(within_info)){
+    stop("Dublicate within_id in within_info found. Make sure within_id is unique")
+  }
+  
+  # check if number of within id matches within columns in data table
+  if(length(unique(data_df$within)) > length(unique(within_info$within_id))){
+    stop("Number of unique within conditions in data table is larger than in within_info table. 
+         \nMake sure all within conditions are included in within_info.")
+  } else if(length(unique(data_df$within)) < length(unique(within_info$within_id))){
+    stop("The within_info table contains more unique within_ids than the data table does. 
+         \nMake sure the within column in the data table is coded correctly and the 
+         within_info table contains only relevant within conditions")
+  }
+}
+
+
+# This function takes a data_[i] list element and checks its entire structure, 
+# including the column names of the sub-element data frames 
+check_data_list <- function(data_i){
+  
+  # check if input is a list
+  if (inherits(data_i, "list") == FALSE)
+  {
+    stop("data_NUMBER object must be a list")
+  } 
+  
+  # check if list contains correct elements
+  correct_elements_in_data_list(data_i)
+  
+  # check if each element in data_i list is a df and contains required columns
+  correct_cols_in_task_info(data_i$task_info)
+  correct_cols_in_overview_info(data_i$overview_info)
+  correct_cols_in_within_info(data_i$within_info)
+  correct_cols_in_data(data_i$data)
+  
+  # check if number of within condition in data equals number of within_ids in within_info
+  correct_n_of_withinid(data_i$within_info, data_i$data)
 }
 
 
