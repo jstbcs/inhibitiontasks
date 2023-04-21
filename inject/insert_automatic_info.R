8d9670b724302b6982640eb4b630f4808f136c7b
MadlenHoffstadt <80783145+MadlenHoffstadt@users.noreply.github.com>
2023-04-21 11:14:09 +0200

    add condition_table info

--- inject/insert_automatic_info.R
+++ inject/insert_automatic_info.R
@@ -37,10 +37,10 @@ add_missing_info <- function(entry_list) {
           df_cond <- filter_condition(entry_list[[i]][[j+1]][[k+2]]$observation_table, cond = cond)
           
           # fill in missing info for condition_table
-          # condition_info$percentage_congruent <- get_per_congr(df_cond)
-          # condition_info$percentage_neutral <- get_perc_neutral(df_cond)
-          # condition_info$mean_obs_pp <- get_mean_obs(df_cond)
-          # condition_info$n_obs <- get_n_obs(df_cond)
+          entry_list[[i]][[j+1]][[k+2]]$condition_table$percentage_congruent[cond] <- get_per_congr(df_cond)
+          entry_list[[i]][[j+1]][[k+2]]$condition_table$percentage_neutral[cond] <- get_perc_neutral(df_cond)
+          entry_list[[i]][[j+1]][[k+2]]$condition_table$mean_obs_per_participant[cond] <- get_mean_obs(df_cond)
+          entry_list[[i]][[j+1]][[k+2]]$condition_table$n_obs[cond] <- get_n_obs(df_cond)
           
         }
       }
@@ -51,19 +51,5 @@ add_missing_info <- function(entry_list) {
 
 
 
-# loop over studies; 
-  # loop over each data element: 
-    # remove practice trials: 
-    # df_test <- remove_practice(df)
-    # for overview_info: 
-      # overview_info$n_participants <- get_n(df_test)
-      # overview_info$n_blocks <- get_n_blocks(df_test)
-      # overview_info$n_trials <- get_n_trials(df_test)
-      # overview_info$neutral_trials <- get_neutral_trials(df_test)
-    # for condition_info: loop over each value of condition in df
-      # create seperate df containing only respective condition (df_cond)
-      # condition_info$percentage_congruent <- get_per_congr(df_cond)
-      # condition_info$percentage_neutral <- get_perc_neutral(df_cond)
-      # condition_info$mean_obs_pp <- get_mean_obs(df_cond)
-      # condition_info$n_obs <- get_n_obs(df_cond)
+
 
