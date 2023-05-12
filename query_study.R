024c6724ab3c22ace7070a0c3f6468d09d417c54
MadlenHoffstadt <80783145+MadlenHoffstadt@users.noreply.github.com>
2023-05-12 15:01:11 +0200

    Delete query_study.R

--- query_study.R
+++
@@ -1,3 +0,0 @@
-# write functions that query one table and return info of ONLY THAT TABLE. Then connect them properly
-# Write "retrieve" functions based on a list of the id infos
-# And also a combination of lists!
