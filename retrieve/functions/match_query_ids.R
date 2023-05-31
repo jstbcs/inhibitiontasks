f4bd40f933ea019094a0a50dea0aab9308685f4b
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-05-31 12:19:32 +0200

    quicker table query

--- retrieve/functions/match_query_ids.R
+++ retrieve/functions/match_query_ids.R
@@ -1,11 +1,16 @@
 extract_table_ids <- function(conn, table, name = NULL){
   # TODO: Replace this with SQL-Language
-  
-  data = as.data.frame(tbl(conn, table))
-  ids = data %>% 
-    select(ends_with("_id"))
+  which_columns_exist_query = paste0("PRAGMA table_info(", table, ")")
+  table_columns = dbGetQuery(conn, which_columns_exist_query)
+  ends_in_id =  table_columns$name[grep("_id$", table_columns$name)]
+
+  sql_statement = paste0("SELECT ", paste(ends_in_id, collapse = ", "), " FROM ", table)
+
+  ids = dbGetQuery(conn, sql_statement)
   
   if (!is.null(name)){
+    data = as.data.frame(tbl(conn, table))
+    
     ids = ids %>% select(contains({{name}}))
   }
   
